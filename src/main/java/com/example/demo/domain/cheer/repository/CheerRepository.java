fee2456de7f58e9e288f5bd93a2e8d9964b49696
leechungheon <leechung6311@gmail.com>
2025-06-02 20:35:23 +0900

    fix: JOIN FETCH 추가

--- src/main/java/com/example/demo/domain/cheer/repository/CheerRepository.java
+++ src/main/java/com/example/demo/domain/cheer/repository/CheerRepository.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 public interface CheerRepository extends JpaRepository<CheerMessage, Long> {
 
-    List<CheerMessage> findByStory_StoryId(Long storyId); // 특정 사연 ID에 해당하는 응원 메시지 목록 조회
+    @Query("SELECT c FROM CheerMessage c JOIN FETCH c.user JOIN FETCH c.category WHERE c.story.storyId = :storyId")
+    List<CheerMessage> findByStory_StoryId(@Param("storyId") Long storyId);
 
     @Query(value = "SELECT * FROM cheer_message WHERE category_id = :catId ORDER BY RAND() LIMIT 1", nativeQuery = true)
     CheerMessage findRandomByCategory(@Param("catId") Long categoryId);
